133c442e5810e8925789ce175b5787274064212d
Ankit Kumar <talk2ankit007@gmail.com>
2024-09-24 00:20:33 +0530

    added unit tests

--- countdown_timer/countdown/tests.py
+++ countdown_timer/countdown/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .forms import TimerForm
 
-# Create your tests here.
+class TimerFormTests(TestCase):
+    def test_valid_form(self):
+        form = TimerForm(data={'hours': 0, 'minutes': 1, 'seconds': 30})
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form = TimerForm(data={'hours': 0, 'minutes': 0, 'seconds': 0})
+        self.assertFalse(form.is_valid())
+
+class CountdownViewTests(TestCase):
+    def test_view_get(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Countdown Timer")
